d0c3adede54a46dddf35b3e1341798c6679cf80b
GustavoIshikaw <Gustavo.ishikaw@gmail.com>
2024-10-22 22:05:55 -0300

    Commit inicial

---
+++ src/com/exemplo/pacote/Teste.java
@@ -0,0 +1,13 @@
+package com.exemplo.pacote;
+
+public class Teste {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.setNome("Bobby");
+        animal.fazerSom();
+
+
+    }
+
+
+}
